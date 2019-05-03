9f85b6b9e457fcf316386cbcda712b562fd0d567
marcelo <om4r.gonzalez@gmail.com>
2019-05-03 15:24:11 -0300

    Depurando el servicio para calcular la cantidad total de combos a preparar

--- server/server_pedido/routes/pedido.js
+++ server/server_pedido/routes/pedido.js
@@ -520,6 +520,8 @@ app.get('/pedido/resumen_cantidad_combos/', async function(req, res) {
                                     conPreferencias: tienePreferencias
                                 });
                             } else {
+                                console.log('El indice del vector productos vale ' + indice);;
+                                console.log(productos[indice])
                                 productos[indice].cantidad = productos[indice].cantidad + 1;
                             }
                         }
