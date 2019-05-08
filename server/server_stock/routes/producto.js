41b9c3961e877c3ee335ae7bf441694e8a10ed1d
Marcelo Gonzalez <om4r.gonzalez@gmail.com>
2019-05-08 11:25:40 -0300

    Se agrego un metodo para buscar productos de acuerdo a los parametros que envia el usuario

--- server/server_stock/routes/producto.js
+++ server/server_stock/routes/producto.js
@@ -555,6 +555,40 @@ app.get('/producto/listar_productos_vigentes/', async function(req, res) {
         });
 });
 
+app.post('/producto/buscar/', async function(req, res) {
+
+    let parametros = 'nombreProducto: acelga';
+
+    Producto.findOne()
+        .where(parametros)
+        .exec(async(err, producto) => {
+            if (err) {
+                console.log('La busqueda de un producto por su id ( ' + req.body.idProducto + ' ) produjo un error');
+                console.log(err.message);
+                return res.json({
+                    ok: false,
+                    message: 'La busqueda de un producto por su id produjo un error',
+                    producto: null
+                });
+            }
+
+            if (producto == null) {
+                console.log('La buesqueda de un producto por su id ( ' + req.body.idProducto + ' ) no devolvio resultados');
+                return res.json({
+                    ok: false,
+                    message: 'La busqueda de un producto por su id ( ' + req.body.idProducto + ' ) no devolvio resultados',
+                    producto: null
+                });
+            }
+
+            res.json({
+                ok: true,
+                message: 'Devolviendo resultado',
+                producto
+            });
+        });
+});
+
 
 
 app.post('/producto/obtener_producto/', async function(req, res) {
